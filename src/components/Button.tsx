089cc834656c0ddd2f221976d1f5b37f6aa7a9ca
Basic Mirko <bejzik8@gmail.com>
2022-10-28 00:45:13 +0100

    Created test Button component.

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    height: 24px;
+    padding: 4px 20px;
+    line-height: 16px;
+    font-size: 12px;
+    border-radius: 1000px;
+    border: none;
+    color: white;
+    background-color: blueviolet;
+    cursor: pointer;
+`
