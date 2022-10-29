442ca3d86c8751b4b7e7d3d9a97e6c7ecdd93ef0
Basic Mirko <bejzik8@gmail.com>
2022-10-29 08:15:06 +0100

    Updated Storybook configuration.

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,5 +8,13 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    })
+    return config
+  }
 }
