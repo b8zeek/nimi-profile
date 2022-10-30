4c8deed3756dd47d93d4b75f2d8e6ba18ba84371
Basic Mirko <bejzik8@gmail.com>
2022-10-30 07:43:44 +0100

    Updated NimiSection's story.

--- src/components/NimiSection/NimiSection.stories.tsx
+++ src/components/NimiSection/NimiSection.stories.tsx
@@ -7,18 +7,12 @@ export default {
   title: 'NimiSection',
   component: NimiSection,
   decorators: [
-    Story => <Container><Content><Story /></Content></Container>]
+    Story => <Content><Story /></Content>]
 } as ComponentMeta<typeof NimiSection>
 
-const Template: ComponentStory<typeof NimiSection> = (args) => <NimiSection>Test NimiSection</NimiSection>;
+const Template: ComponentStory<typeof NimiSection> = (args) => <NimiSection>Mirko Basic</NimiSection>;
 
 export const Primary = Template.bind({})
-
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #222;
-`
 
 const Content = styled.div`
   max-width: 570px;
