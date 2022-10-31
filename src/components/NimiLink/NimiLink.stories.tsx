a0ebef2521b3a1637e6766d9bdb60678ed4588c2
Basic Mirko <bejzik8@gmail.com>
2022-10-31 23:25:03 +0100

    Created NimiLink story.

---
+++ src/components/NimiLink/NimiLink.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import styled from 'styled-components'
+
+import { NimiLink } from './NimiLink'
+
+export default {
+    title: 'NimiLink',
+    component: NimiLink,
+    decorators: [Story => <Container><Story/></Container>]
+} as ComponentMeta<typeof NimiLink>
+
+const Template: ComponentStory<typeof NimiLink> = (args) => <NimiLink {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+    children: 'Bejzik',
+    onClick: () => console.log('Link click action.')
+}
+
+const Container = styled.div`
+    max-width: 570px;
+    margin: 0 auto;
+`
