d00c0a54a96df0f0339b19c6461ed6b57a30fd1f
Basic Mirko <bejzik8@gmail.com>
2022-11-03 12:08:41 +0100

    Updated ThemeProvider.

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -6,6 +6,36 @@ import { NIMI_CARDS_WIDTH } from '../constants'
 import { NimiThemeType } from '../types'
 import { NimiTheme } from '../types/NimiTheme'
 
+const GlobalStyles = createGlobalStyle<{ theme: NimiTheme }>`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+
+  html, body, input, label, textarea, button,  h1, h2, h3, h4, h5, h6, p, a {
+    font-family: 'Archivo', sans-serif;
+  }
+
+  body {
+    min-height: 100vh;
+    position: relative;
+    ${({ theme }) => getTextColor(theme.type)}
+    ${({ theme }) => getPageBackground(theme.type)}
+  }
+
+  button { user-select: none; }
+
+  a { text-decoration: none; }
+`
+
 const GradientAnimation = keyframes`
   0% {
     background-position: 0;
@@ -14,6 +44,22 @@ const GradientAnimation = keyframes`
     background-position: 100%;
   }
 `
+
+type ThemeProviderProps = {
+  theme: NimiTheme
+  children: ReactNode
+}
+
+export function ThemeProvider({ theme = { type: NimiThemeType.DEVCON }, children }: ThemeProviderProps) {
+
+  return (
+    <StyledTheme theme={theme}>
+      {children}
+      <GlobalStyles />
+      {getBackgroundImage(theme.type)}
+    </StyledTheme>
+  )
+}
 
 function getPageBackground(themeType: string) {
   switch (themeType) {
@@ -53,83 +99,34 @@ function getTextColor(themeType: string) {
   }
 }
 
-const GlobalStyles = createGlobalStyle<{ theme: NimiTheme }>`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+const getBackgroundImage = (themeType: string) => {
+  switch (themeType) {
+    case NimiThemeType.NIMI:
+      return (
+        <BackgroundContainer>
+          <BackgroundImage src='https://bafybeif5c6xz6ryiyrtm4r6amwiftwrw2kf3llipy6dco27hp3ilftthtm.ipfs.dweb.link/nimi-header-background.d73a42cfaca4acf944f4.png' />
+        </BackgroundContainer>
+      )
+    case NimiThemeType.DEVCON:
+      return (
+        <BackgroundContainer>
+          <BackgroundImage
+            shouldSpin
+            src='https://ipfs.io/ipfs/QmVa4QEciC16UpTcALJGk1U5Tn3qNZLNt872gZsnXByTVE?filename=rays.svg'
+          />
+        </BackgroundContainer>
+      )
+    case NimiThemeType.INFINITE:
+      return null
+    case NimiThemeType.DAIVINITY:
+      return (
+        <BackgroundContainer>
+          <BackgroundImage src={daivinityBackground} />
+        </BackgroundContainer>
+      )
+    default:
+      return null
   }
-
-  html {
-    font-size: 16px;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  }
-
-  html, body, input, label, textarea, button {
-    font-family: 'Archivo', sans-serif;
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-    font-family: 'Archivo', sans-serif;
-  }
-
-  body {
-    min-height: 100vh;
-    position: relative;
-    ${({ theme }) => getTextColor(theme.type)}
-    ${({ theme }) => getPageBackground(theme.type)}
-  }
-
-  button { user-select: none; }
-
-  a { text-decoration: none; }
-`
-
-type ThemeProviderProps = {
-  theme: NimiTheme
-  children: ReactNode
-}
-
-export function ThemeProvider({ theme = { type: NimiThemeType.DAIVINITY }, children }: ThemeProviderProps) {
-  const getBackgroundImage = (themeType: string) => {
-    switch (themeType) {
-      case NimiThemeType.NIMI:
-        return (
-          <BackgroundContainer>
-            <BackgroundImage src='https://bafybeif5c6xz6ryiyrtm4r6amwiftwrw2kf3llipy6dco27hp3ilftthtm.ipfs.dweb.link/nimi-header-background.d73a42cfaca4acf944f4.png' />
-          </BackgroundContainer>
-        )
-      case NimiThemeType.DEVCON:
-        return (
-          <BackgroundContainer>
-            <BackgroundImage
-              shouldSpin
-              src='https://ipfs.io/ipfs/QmVa4QEciC16UpTcALJGk1U5Tn3qNZLNt872gZsnXByTVE?filename=rays.svg'
-            />
-          </BackgroundContainer>
-        )
-      case NimiThemeType.INFINITE:
-        return null
-      case NimiThemeType.DAIVINITY:
-        return (
-          <BackgroundContainer>
-            <BackgroundImage src={daivinityBackground} />
-          </BackgroundContainer>
-        )
-      default:
-        return null
-    }
-  }
-
-  return (
-    <StyledTheme theme={theme}>
-      {children}
-      <GlobalStyles />
-      {getBackgroundImage(theme.type)}
-    </StyledTheme>
-  )
 }
 
 const Spin = keyframes`
