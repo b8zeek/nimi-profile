4ec0d1183e008627753785221995737c3ff8bde1
Basic Mirko <bejzik8@gmail.com>
2022-11-04 07:32:43 +0100

    Updated Storybook decorators.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,12 @@
 import { ThemeProvider } from '../src/theme/ThemeProvider'
 
-export const decorators = [
-  Story => (
-    <ThemeProvider>
-      <Story />
-    </ThemeProvider>
-  )
-]
+// export const decorators = [
+//   Story => (
+//     <ThemeProvider>
+//       <Story />
+//     </ThemeProvider>
+//   )
+// ]
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
