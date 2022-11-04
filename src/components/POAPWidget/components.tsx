8cc9c38228f5bb89526d33d5e71a0920a81b183b
Basic Mirko <bejzik8@gmail.com>
2022-11-04 07:18:49 +0100

    Updated POAPWidget sub-components.

--- src/components/POAPWidget/components.tsx
+++ src/components/POAPWidget/components.tsx
@@ -2,26 +2,21 @@ import styled from 'styled-components'
 
 import { ReactComponent as ExternalLinkSvg } from '../../assets/svg/common/external-link.svg'
 import { NimiLink } from '../../components/NimiLink'
-
-export const Section = styled.section<{ padding?: string }>`
-  display: flex;
-  flex-direction: column;
-  padding: ${({ padding }) => (padding ? padding : '38px 36px')};
-  background: #ffffff;
-  box-shadow: 0px 8px 35px #e9e0ff;
-  border-radius: 25px;
-  width: 100%;
-  margin-bottom: 40px;
-`
+import { NimiThemeType } from '../../types'
 
 export const Container = styled.div`
   margin-bottom: 25px;
 `
 
-export function POAPHeading({ showFullCollection }: { showFullCollection: () => void }) {
+type POAPHeadingProps = {
+  showFullCollection: () => void
+  themeType: NimiThemeType
+}
+
+export function POAPHeading({ showFullCollection, themeType }: POAPHeadingProps) {
   return (
     <POAPHeadingContainer>
-      <Title>POAPs</Title>
+      <Title themeType={themeType}>POAPs</Title>
       <NimiLink lightButton onClick={showFullCollection}>
         Full Collection <ExternalLinkSvg />
       </NimiLink>
@@ -36,10 +31,12 @@ const POAPHeadingContainer = styled.div`
   margin-bottom: 16px;
 `
 
-export const Title = styled.h2`
+export const Title = styled.h2<{ themeType: NimiThemeType }>`
   line-height: 26px;
   font-size: 26px;
+  font-family: 'Archivo', sans-serif;
   font-weight: 700;
+  ${({ themeType }) => getTextColor(themeType)}
 `
 
 export const POAPList = styled.div`
@@ -53,3 +50,20 @@ export const POAPItem = styled.img`
   border-radius: 50%;
   margin-right: -4%;
 `
+
+function getTextColor(themeType: NimiThemeType) {
+  switch (themeType) {
+    case NimiThemeType.NIMI:
+      return 'color: rgba(33, 33, 35, 0.65);'
+    case NimiThemeType.DEVCON:
+      return 'color: rgba(33, 33, 35, 0.65);'
+    case NimiThemeType.RAAVE:
+      return 'color: rgba(190, 215, 132, 1);'
+    case NimiThemeType.INFINITE:
+      return 'color: #fff;'
+    case NimiThemeType.DAIVINITY:
+      return 'color: #212123;'
+    default:
+      return null
+  }
+}
