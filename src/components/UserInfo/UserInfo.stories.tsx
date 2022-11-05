90bb9cf209230c0e31c87d2c32ad7d4662f86ec8
Basic Mirko <bejzik8@gmail.com>
2022-11-05 15:51:51 +0100

    Updated UserInfo story.

--- src/components/UserInfo/UserInfo.stories.tsx
+++ src/components/UserInfo/UserInfo.stories.tsx
@@ -18,15 +18,29 @@ export default {
 
 const Template: ComponentStory<typeof UserInfo> = args => <UserInfo {...args} />
 
-export const Primary = Template.bind({})
-Primary.args = {
+const getArgs = (themeType: NimiThemeType) => ({
   displayName: '🍀 Bejzik 🍀',
   description: 'Experience technologist focused on crafting future-proof web applications.',
   ensAddress: '0xb492873D940dAc02B5021dFF82282d8374509582',
   ensName: 'bejzik.eth',
   setProfilePhotoRotated: () => console.log('Rotate profile photo click.'),
-  themeType: NimiThemeType.RAAVE
-}
+  themeType: themeType
+})
+
+export const Default = Template.bind({})
+Default.args = getArgs(NimiThemeType.DAIVINITY)
+
+export const Devcon = Template.bind({})
+Devcon.args = getArgs(NimiThemeType.DEVCON)
+
+export const Raave = Template.bind({})
+Raave.args = getArgs(NimiThemeType.RAAVE)
+
+export const Daivinity = Template.bind({})
+Daivinity.args = getArgs(NimiThemeType.DAIVINITY)
+
+export const Infinite = Template.bind({})
+Infinite.args = getArgs(NimiThemeType.INFINITE)
 
 const Container = styled.div`
   max-width: 570px;
